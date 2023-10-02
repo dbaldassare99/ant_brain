d0edc69453d3fc79d9a2f43c983c210715221d03
Graham Seamans <seamansgraham@gmail.com>
2023-10-02 09:38:47 -0700

    working jacobian iteration

--- recursive_planner/newton.py
+++ recursive_planner/newton.py
@@ -9,23 +9,24 @@ def get_action(net: Brain, obs, goal):
     return grad
 
 
-def get_midpoint(net: Brain, obs, goal):
+def get_midpoint(
+    net: Brain, obs: torch.Tensor, goal: torch.Tensor, noise: torch.Tensor
+):
     net = net.eval()
-    batch = obs.shape[0]
-    noise = torch.randn(batch, 16)
     midpoint_jacobian = vmap(
         jacrev(lambda x, y, z: net.unbatched_forward(x, y, z).midpoint_optim_loss(), 2)
     )
-    for _ in range(10):
-        print(net(obs, goal, noise).midpoint_optim_loss())
-        print(obs.shape, goal.shape, noise.shape)
+    for _ in range(100):
+        print(net(obs, goal, noise).midpoint_optim_loss().mean())
         grad = midpoint_jacobian(obs, goal, noise)
-        grad = grad.unsqueeze(1)
+        grad = grad.squeeze(1) * -1
         noise = noise - grad
 
 
 frame_1 = torch.randn(10, 224, 240, 3)
 frame_2 = torch.randn(10, 224, 240, 3)
+batch = frame_1.shape[0]
+noise = torch.randn(batch, 16)
 brain = Brain()
 
-grad = get_midpoint(brain, frame_1, frame_2)
+grad = get_midpoint(brain, frame_1, frame_2, noise)
